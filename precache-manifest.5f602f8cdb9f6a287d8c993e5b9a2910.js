577305d98eb95c2ebb2b917d6314610b9b52e8b5
Tabitha <andert97@uw.edu>
2020-02-21 23:15:28 -0800

    Updates

---
+++ precache-manifest.5f602f8cdb9f6a287d8c993e5b9a2910.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "832ce42dd3ac8dbd928d722f0a36c9fe",
+    "url": "/capstone-tanc-3/landing-page/index.html"
+  },
+  {
+    "revision": "7feb720f5125ffac64a7",
+    "url": "/capstone-tanc-3/landing-page/static/css/main.192bb317.chunk.css"
+  },
+  {
+    "revision": "2689ddee34a6f073f92f",
+    "url": "/capstone-tanc-3/landing-page/static/js/2.3d629cfc.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/capstone-tanc-3/landing-page/static/js/2.3d629cfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7feb720f5125ffac64a7",
+    "url": "/capstone-tanc-3/landing-page/static/js/main.f328df11.chunk.js"
+  },
+  {
+    "revision": "3ea91cfc8b79dc30c9c2",
+    "url": "/capstone-tanc-3/landing-page/static/js/runtime-main.66ab64c0.js"
+  },
+  {
+    "revision": "35c6d61e52da636cf0d88f35f803b131",
+    "url": "/capstone-tanc-3/landing-page/static/media/AnuraagTemp.35c6d61e.jpg"
+  },
+  {
+    "revision": "471c7f614b11a772df76f5b6e60b0dd2",
+    "url": "/capstone-tanc-3/landing-page/static/media/ChrisTemp.471c7f61.jpg"
+  },
+  {
+    "revision": "0d53c6a4fe870bbd53c11bf6b21bfec6",
+    "url": "/capstone-tanc-3/landing-page/static/media/NavedTemp.0d53c6a4.jpg"
+  },
+  {
+    "revision": "b4d9c2d45d0211e48a08222cf161efdf",
+    "url": "/capstone-tanc-3/landing-page/static/media/Scene1.b4d9c2d4.png"
+  },
+  {
+    "revision": "571daa042ef5d8e1092dee22aec08d3a",
+    "url": "/capstone-tanc-3/landing-page/static/media/Scene2.571daa04.png"
+  },
+  {
+    "revision": "3bb950f58850ad2fd7bf5f95ae6faac3",
+    "url": "/capstone-tanc-3/landing-page/static/media/Scene3.3bb950f5.png"
+  },
+  {
+    "revision": "39ae7b9b3c0e76e247b355ef6af15f91",
+    "url": "/capstone-tanc-3/landing-page/static/media/TabithaTemp.39ae7b9b.jpg"
+  }
+]);
